b923c4a09ffdb04a44220c401e0cd51fd8fbbdcd
Emanuelle <emanuellejs1999@gmail.com>
2021-01-09 11:22:56 -0600

    Se terminó el issue de gestión de usuarios

---
+++ Registro_y_Control_de_Extintores/Registro_y_Control_de_Extintores/Models/UsuarioModelo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Registro_y_control_de_extintores.Models
+{
+    class usuario
+    {
+        public int Id { get; set; }
+
+        public int id_centro { get; set; }
+
+        public int cedula { get; set; }
+
+        public string correo { get; set; }
+
+        public string clave { get; set; }
+
+        public int administrador { get; set; }
+
+        public usuario()
+        {
+        }
+
+        public usuario(int id, int id_centro, int cedula, string correo, string clave, int administrador) {
+            this.Id = id;
+            this.id_centro = id_centro;
+            this.cedula = cedula;
+            this.correo = correo;
+            this.clave = clave;
+            this.administrador = administrador;
+        }
+
+    }
+}
